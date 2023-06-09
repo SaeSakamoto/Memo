1aa7b3ae0abdbaf1deb86b5c108698284ef7ceea
Sae SAKAMOTO <sae.sakamoto@bric.jp>
2023-06-09 15:22:29 +0900

    Initial Commit

---
+++ MemoApp/MemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoAppApp.swift
+//  MemoApp
+//
+//  Created by Sae Sakamoto on 2023/06/09.
+//
+
+import SwiftUI
+
+@main
+struct MemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
